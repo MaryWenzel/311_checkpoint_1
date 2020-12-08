df5a9c16b32ca04e8798c1835ccee93afc6e21ef
MaryWenzel <wenzel.mary1993@gmail.com>
2020-12-08 11:52:34 -0600

    finished assignment

---
+++ controllers/users.js
@@ -0,0 +1,82 @@
+const users = require('../data/index');
+// If I want to use sampleUser
+// const sampleUser = require('../data/sampleUser');
+
+// GET USERS
+const listUsers = (req, res) => {
+    res.json(users)
+};
+
+// GET USER BY ID
+const showUser = (req, res) => {
+    const userId = req.params.id
+    for(let user of users){
+        if (user.id === parseInt(userId)){
+            res.json(user)
+        } else{
+            return res.status(400).json({ msg:`No user with the id of ${userId}` })
+        }
+    }
+};
+
+// POST USER
+const createUser = (req, res) => {
+    const newUser = {
+        id: users.length + 1,
+        name: req.body.name,
+        username: req.body.username,
+        email: req.body.email
+    }
+
+    if(!newUser.name || !newUser.username || !newUser.email){
+        return res.status(400).json({ msg:"Please enter name, username and email." })
+    }
+
+    // adds sampleUser to end of array
+    // users.push(sampleUser)
+    users.push(newUser)
+    res.json(users)
+};
+
+// PUT/ UPDATE USER
+const updateUser = (req, res) => {
+    const found = users.some(user => user.id === parseInt(req.params.id));
+  
+    if(found){
+      const updUser = req.body;
+      users.forEach(user => {
+          if(user.id === parseInt(req.params.id)){
+          user.name = updUser.name ? updUser.name : user.name;
+          user.username = updUser.username ? updUser.username : user.username;
+          user.email = updUser.email ? updUser.email : user.email;
+           
+          res.json({ msg: "User Updated", user })
+        }
+      });
+    } else{
+      res.status(400).json({ msg: `No user with the id of ${req.params.id}` });
+    }
+  
+  };
+
+// DELETE USER
+const deleteUser = (req, res) => {
+    const found = users.some(user => user.id === parseInt(req.params.id));
+  
+    if(found){
+      res.json({
+         msg: "Deleted User",
+         users: users.filter(user => user.id !== parseInt(req.params.id))
+        });
+    }else {
+      res.status(400).json({ msg: `no member with id of ${req.params.id}` })
+    }
+  };
+
+  module.exports = {
+    listUsers,
+    showUser,
+    createUser,
+    updateUser,
+    deleteUser
+  };
--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const express = require('express')
+const bodyParser = require("body-parser");
+
+const users = require('./routes/users')
+
 const app = express()
-const port = process.env.PORT || 4000
+app.use(bodyParser.json());
+app.use(users)
+
 
 app.get('/', (req, res) => res.send('default route'))
+
+const port = process.env.PORT || 4000
 
 app.listen(port, () => {
   console.log('app is listening on:', port)
